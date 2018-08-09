cf0831c2301670e978aacff68d24223db1bb1b98
舟木正憲 <funakimfunaki@gmail.com>
2018-08-09 22:52:18 +0900

    投稿機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
+  post 'posts/create' => 'posts#create'
   get '/' => 'home#top'
   get 'about' => 'home#about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
