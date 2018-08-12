a36e29d28f11e94d4aaa242ba0fc7a3c363b2ee9
舟木正憲 <funakimfunaki@gmail.com>
2018-08-12 12:26:08 +0900

    ユーザー機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  post 'users/:id/update' => 'users#update'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/create' => 'users#create'
+  get 'signup' => 'users#new'
+  get 'users/index'
+  get 'users/:id' => 'users#show'
+
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
@@ -6,6 +13,7 @@ Rails.application.routes.draw do
   get 'posts/:id/edit' => 'posts#edit'
   post 'posts/:id/update' => 'posts#update'
   post 'posts/:id/destroy' => 'posts#destroy'
+
   get '/' => 'home#top'
   get 'about' => 'home#about'
 end
