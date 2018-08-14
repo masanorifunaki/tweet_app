2c9bb154094c44a6fca47f8fb747c6ea7eeb373a
舟木正憲 <funakimfunaki@gmail.com>
2018-08-14 23:59:56 +0900

    インスタンスメソッドの実行

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 # Comment
 class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 }
+  validates :user_id, presence: true
+
+  def user
+    User.find_by(id: user_id)
+  end
+
 end
