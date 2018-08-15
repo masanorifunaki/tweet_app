1358e49d8dbcc0d73184764fc9681ff4259773d0
舟木正憲 <funakimfunaki@gmail.com>
2018-08-15 23:39:32 +0900

    「投稿」と「ユーザー」を紐付けの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+
+  def posts
+    Post.where(user_id: id)
+  end
+
 end
